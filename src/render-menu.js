77d967b85f478fe15c3487600ebf5aca318d6a7a
McKathlin <mckathlin@gmail.com>
2024-12-02 22:42:36 +0000

    Place menu photos and headings

--- src/render-menu.js
+++ src/render-menu.js
@@ -1,9 +1,46 @@
 import doc from "./lib/doc.js";
+import lorem from "./lib/lorem.js";
+
+import sandwichImage from "./img/roast-beef-sandwich.jpg";
+import saladImage from "./img/berry-fruit-salad.jpg";
+import cocoaImage from "./img/hot-cocoa.jpg";
+import pieImage from "./img/plum-pie.jpg";
 
 function render(contentNode) {
     contentNode.append(
-        doc.h2("Menu"),
-        doc.p("This is the menu page."),
+        doc.make(".section", [
+            doc.img({ src: sandwichImage }),
+            doc.make(".text-block", [
+                doc.make("h2.left", "Sandwiches"),
+            ]),
+        ]),
+    );
+
+    contentNode.append(
+        doc.make(".section", [
+            doc.make(".text-block", [
+                doc.make("h2.right", "Sides"),
+            ]),
+            doc.img({ src: saladImage }),
+        ]),
+    );
+
+    contentNode.append(
+        doc.make(".section", [
+            doc.img({ src: cocoaImage }),
+            doc.make(".text-block", [
+                doc.make("h2.left", "Beverages"),
+            ]),
+        ]),
+    );
+
+    contentNode.append(
+        doc.make(".section", [
+            doc.make(".text-block", [
+                doc.make("h2.right", "Sweet Treats"),
+            ]),
+            doc.img({ src: pieImage }),
+        ]),
     );
 }
 
