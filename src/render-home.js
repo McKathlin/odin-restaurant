d8b823b68de24ed2deeabf54af992026ead881d7
McKathlin <mckathlin@gmail.com>
2024-12-02 18:29:11 +0000

    Group home page text into text-block div

--- src/render-home.js
+++ src/render-home.js
@@ -6,10 +6,12 @@ import shopWindowImage from "./img/christmas-shop-window.jpg";
 function render(contentNode) {
     contentNode.append(
         doc.img({ className: "image-left", src: shopWindowImage }),
-        doc.h2("Do you need a little Christmas?"),
-        doc.p("Visit the 34th Street Café for Christmas cheer, any time of year."),
-        doc.p(lorem(1)),
-        doc.p("Conveniently and meaningfully located on 1225 34th Street, we're open from 10:00am to 4:00pm.")
+        doc.div({ className: "text-block", children: [
+            doc.h2("Do you need a little Christmas?"),
+            doc.p("Visit the 34th Street Café for Christmas cheer, any time of year."),
+            doc.p(lorem(1)),
+            doc.p("Conveniently and meaningfully located on 1225 34th Street, we're open from 10:00am to 4:00pm."),
+        ]}),
     );
 }
 
