9aa62d4705cefb924776a2c13ef17eea2ea88328
McKathlin <mckathlin@gmail.com>
2024-12-02 20:33:50 +0000

    Fill About section

--- src/render-about.js
+++ src/render-about.js
@@ -1,9 +1,35 @@
 import doc from "./lib/doc.js";
+import lorem from "./lib/lorem.js";
+
+import cookiesImage from "./img/milk-and-cookies.jpg";
+import shopWindowImage from "./img/christmas-shop-window.jpg";
 
 function render(contentNode) {
     contentNode.append(
-        doc.h2("About Us"),
-        doc.p("This is the about page."),
+        doc.make(".section", [
+            doc.img({ src: cookiesImage }),
+            doc.make(".text-block", [
+                doc.h2("About Us"),
+                doc.p("Our mission is to warm up your inner child with Christmas wonder, all year round."),
+                doc.p(lorem(20)),
+                doc.p(lorem(21)),
+            ]),
+        ]),
+        doc.make(".section", [
+            doc.make(".text-block", [
+                doc.h2("Our History"),
+                doc.p([
+                    "Our name is inspired by the classic movie ",
+                    doc.em("Miracle on 34th Street"), ". ",
+                    lorem(22),
+                ]),
+                doc.p(lorem(23)),
+                doc.p(lorem(24)),
+                doc.p(lorem(25)),
+                doc.make(".filigree"),
+            ]),
+            doc.img({ src: shopWindowImage }),
+        ]),
     );
 }
 
